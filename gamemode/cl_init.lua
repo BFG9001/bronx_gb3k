c939ccf0ca376dfd211e8dfe4e423db4b68009d5
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-14 14:16:45 -0700

    error fixing

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -2,10 +2,13 @@ include( 'shared.lua' )
 
 local LASTGAMESTATE = ""
 hook.Add("Think", "Bronx_GameStateSynchro", function()
-	local changed = !(LASTGAMESTATE == GetGlobalString(CURRENT_GAMESTATE, "Debug"))
+	local changed = (LASTGAMESTATE ~= GetGlobalString(CURRENT_GAMESTATE, "Debug"))
+	LASTGAMESTATE = GetGlobalString( CURRENT_GAMESTATE, "Debug" )
+	print(changed)
 	if changed then
-		if GAMEMODE.GameStates[LASTGAMESTATE].StateFinish then GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() end
+		if GAMEMODE.GameStates[LASTGAMESTATE] and GAMEMODE.GameStates[LASTGAMESTATE].StateFinish then 
+			GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() 
+		end
 		hook.Call("StateBegin", GAMEMODE, nil )
 	end
-	LASTGAMESTATE = GetGlobalString( CURRENT_GAMESTATE, "Debug" )
 end)
