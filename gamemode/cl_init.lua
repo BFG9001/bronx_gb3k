1aee2dad6a8df04398dac8c91c2e60d378a6493b
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-14 14:43:18 -0700

    ARemoved debug code

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -4,7 +4,7 @@ local LASTGAMESTATE = ""
 hook.Add("Think", "Bronx_GameStateSynchro", function()
 	local changed = (LASTGAMESTATE ~= GetGlobalString(CURRENT_GAMESTATE, "Debug"))
 	LASTGAMESTATE = GetGlobalString( CURRENT_GAMESTATE, "Debug" )
-	print(changed)
+	--print(changed)
 	if changed then
 		if GAMEMODE.GameStates[LASTGAMESTATE] and GAMEMODE.GameStates[LASTGAMESTATE].StateFinish then 
 			GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() 
