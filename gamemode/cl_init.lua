ecacb99c81518369f008fa1266d191a6cefde2d5
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-14 13:50:51 -0700

    WOW FUCK

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -2,7 +2,7 @@ include( 'shared.lua' )
 
 local LASTGAMESTATE = ""
 hook.Add("Think", "Bronx_GameStateSynchro", function()
-	local changed = (LASTGAMESTATE == GetGlobalString(CURRENT_GAMESTATE, "Debug"))
+	local changed = !(LASTGAMESTATE == GetGlobalString(CURRENT_GAMESTATE, "Debug"))
 	if changed then
 		if GAMEMODE.GameStates[LASTGAMESTATE].StateFinish then GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() end
 		hook.Call("StateBegin", GAMEMODE, nil )
