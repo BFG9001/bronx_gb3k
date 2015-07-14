5852b4e047f9f292cc1b067e9f51973c9523dcfd
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-14 13:47:46 -0700

    fallback for state system

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -4,7 +4,7 @@ local LASTGAMESTATE = ""
 hook.Add("Think", "Bronx_GameStateSynchro", function()
 	local changed = (LASTGAMESTATE == GetGlobalString(CURRENT_GAMESTATE, "Debug"))
 	if changed then
-		GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish()
+		if GAMEMODE.Gamestates[LASTGAMESTATE].StateFinish then GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() end
 		hook.Call("StateBegin", GAMEMODE, nil )
 	end
 	LASTGAMESTATE = GetGlobalString( CURRENT_GAMESTATE, "Debug" )
