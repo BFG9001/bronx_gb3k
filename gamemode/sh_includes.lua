4ec8c1d45650ab3fc62af4b642c17df16e8f0e98
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-14 17:44:10 -0700

    added sv_spawnsystems.lua to sh_includes file

--- gamemode/sh_includes.lua
+++ gamemode/sh_includes.lua
@@ -2,7 +2,7 @@ AddCSLuaFile()
 
 local client_files = {"cl_postprocess.lua", "cl_basehud.lua"}
 
-local server_files = {"sv_mapsetup.lua", "sv_concommands.lua", "sv_equipment.lua"}
+local server_files = {"sv_mapsetup.lua", "sv_concommands.lua", "sv_equipment.lua", "sv_spawnsystems.lua"}
 
 local shared_files = {"gamestates/debug.lua", "gamestates/preround.lua"}
 
