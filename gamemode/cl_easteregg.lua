8b07286387ea9ed64cbfd62f20f443d94b6aaf5f
Sean Chan <seanchan@vbcookies-imac.attlocal.net>
2015-07-15 15:15:05 -0700

    blanking the easter egg for now.

--- gamemode/cl_easteregg.lua
+++ gamemode/cl_easteregg.lua
@@ -1,14 +1,1 @@
-include( 'shared.lua' )
 
-local LASTGAMESTATE = ""
-hook.Add("Think", "Bronx_GameStateSynchro", function()
-	local changed = (LASTGAMESTATE ~= GetGlobalString(CURRENT_GAMESTATE, "Debug"))
-	--print(changed)
-	if changed then
-		if GAMEMODE.GameStates[LASTGAMESTATE] and GAMEMODE.GameStates[LASTGAMESTATE].StateFinish then 
-			GAMEMODE.GameStates[LASTGAMESTATE]:StateFinish() 
-		end
-		hook.Call("StateBegin", GAMEMODE, nil )
-	end
-	LASTGAMESTATE = GetGlobalString( CURRENT_GAMESTATE, "Debug" )
-end)
